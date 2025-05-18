d80b8ea5b81c7903e1b9cabd7f2e354d330654d7
Sheikh MD. Faysal <sheikh.faysal@niftycoders.com>
2025-05-18 16:30:04 +0600

    postgress added and path resolved

--- Database Scripts/PostgreSQLScript.sql
+++ Database Scripts/PostgreSQLScript.sql
@@ -9,6 +9,7 @@ CREATE TABLE users (
   hashed_password varchar(200) DEFAULT NULL,
   is_active boolean DEFAULT NULL,
   role varchar(45) DEFAULT NULL,
+  phone_number varchar(45) DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
--- database.py
+++ database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+POSTGRES_DATABASE_URL = 'postgresql://postgres:faysal1997@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# This is for sqllite
+# engine = create_engine(POSTGRES_DATABASE_URL, connect_args={'check_same_thread': False})
+
+engine = create_engine(POSTGRES_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
